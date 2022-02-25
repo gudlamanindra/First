659d4b49b92b2e2e144d99df2f7057b6ad3eb5e2
Manindra <gudlamanindra@gmail.com>
2022-02-25 16:50:15 +0530

    TaskNew.java

--- src/test/java/StepDefinition/TaskNew.java
+++ src/test/java/StepDefinition/TaskNew.java
@@ -22,23 +22,6 @@ public class TaskNew {
         else {
         	System.out.println("False");
         }
-//		char c=s4.charAt(3);
-//		System.out.println(c);
-//		if(s4.equals("QWERTY")){
-//			 System.out.println("True");
-//		}
-//		else{
-//			System.out.println("False");
-//		}
-//		if(s4.equalsIgnoreCase("qwerTy")){
-//			System.out.println("True");
-//		}
-//		else{
-//		    System.out.println("False");
-//		}
-//		char c1 = s4.charAt(4);
-//		System.out.println(c1);
-//		int size=s4.length();
-//		System.out.println(size);
+
         }
 }
