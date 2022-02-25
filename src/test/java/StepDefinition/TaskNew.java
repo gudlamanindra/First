53915fe256fa1fa53056434295e7809db925cd0e
Manindra <gudlamanindra@gmail.com>
2022-02-25 15:00:58 +0530

    Updated TaskNew.java file

--- src/test/java/StepDefinition/TaskNew.java
+++ src/test/java/StepDefinition/TaskNew.java
@@ -2,27 +2,26 @@ package StepDefinition;
 public class TaskNew {
 	public static void main(String[] args) {
 		String s4 = "QWERTY";
-		s4= s4.split("R")[0].replace("E","K");
+		s4= s4.split("R")[0].replace("W","K");
         System.out.println(s4);
-        if(s4.equals("QWK")) {
+        if(s4.equals("QKE")) {
         	System.out.println("True");
         }
         else {
             System.out.println("False");
         }
-        if(s4.equalsIgnoreCase("qwk")) {
+        if(s4.equalsIgnoreCase("qke")) {
         	System.out.println("True");
         }
         else{
         System.out.println("False");	
         }
-        if(s4.contains("W")){
+        if(s4.contains("K")){
         	System.out.println("True");
         }
         else {
         	System.out.println("False");
         }
-         
 //		char c=s4.charAt(3);
 //		System.out.println(c);
 //		if(s4.equals("QWERTY")){
