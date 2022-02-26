ce933c072555c74a2da8073baaee896ed0c419d0
Manindra <gudlamanindra@gmail.com>
2022-02-26 15:24:10 +0530

    src/test/java/StepDefinition/StringMethods.java

--- src/test/java/StepDefinition/StringMethods.java
+++ src/test/java/StepDefinition/StringMethods.java
@@ -11,14 +11,7 @@ public static void main(String[] args) throws InterruptedException{
         	System.out.println("False");
         }
 		System.out.println(s1);
-//		s1=s1.replace("k","v");
-//		System.out.println(s1);
-//        int size = s1.length();
-//        System.out.println(size);
-//        s1 = s1.toUpperCase();
-//        System.out.println(s1);
-//        s1 = s1.toLowerCase();
-//        System.out.println(s1);
+
       
         if(s1.equals("jaka")){
 			 System.out.println("True");
@@ -34,17 +27,7 @@ public static void main(String[] args) throws InterruptedException{
 		    System.out.println("False");
 		}
 		
-//		if(s1.contains("av")){
-//			System.out.println("true");
-//		}
-//		else{
-//		    System.out.println("False");
-//		}
-        
-//		char c = s1.charAt(2);
-//		System.out.println(c);
-//		
-//	    
+
 	    Thread.sleep(5000);
 	}   
 }
