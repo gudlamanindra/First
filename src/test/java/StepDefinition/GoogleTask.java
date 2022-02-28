24c7447a0c5ba3486c82c7c09d3c937a9defa630
Manindra <gudlamanindra@gmail.com>
2022-03-01 00:06:54 +0530

    src/test/java/StepDefinition/GoogleTask.java

--- src/test/java/StepDefinition/GoogleTask.java
+++ src/test/java/StepDefinition/GoogleTask.java
@@ -20,7 +20,6 @@ import org.openqa.selenium.chrome.ChromeDriver;
 	    for(int i=1;i<=l.size();i++) {
 		  String text=driver.findElement(By.xpath("(//ul[@jsname='bw4e9b'])[1]/li["+i+"]")).getText(); 
 		  System.out.println(text);
-		  //text=text.split("\n")[0];
 		  if(text.equals("exam")){
 			  driver.findElement(By.xpath("(//ul[@jsname='bw4e9b'])[1]/li["+i+"]")).click();
 			  break;
